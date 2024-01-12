ae2f2f1e24dbd7f662f3ba812e6fb69e99269228
黄典 <870003719@qq.com>
2024-01-12 17:46:25 +0800

    feat: 直接兑换页-基础

--- src/pages/Exchange/index.jsx
+++ src/pages/Exchange/index.jsx
@@ -1,11 +1,56 @@
-import { Button, Space } from 'antd-mobile';
-import { useNavigate } from 'react-router-dom';
+import { Dialog } from 'antd-mobile';
 
-// import styles from './index.module.less';
+import { useUser } from '@/components/UserContext';
+
+import styles from './index.module.less';
+
+const UserPoints = () => {
+  const { userInfo } = useUser();
+  if (!userInfo) return null;
+  return (
+    <>
+      <div className={styles.expend}>消耗168积分</div>
+      <div className={styles.surplus}>
+        当前剩余积分：{userInfo.points} <span>如何获取积分</span>
+      </div>
+    </>
+  );
+};
 
 const Exchange = () => {
-  const navigate = useNavigate();
-  return <Space wrap>Exchange</Space>;
+  const { userInfo, login } = useUser();
+  const handleSummon = () => {
+    if (!userInfo) return login();
+    Dialog.show({
+      content: '是否确认消耗xxx积分进行抽取',
+      closeOnAction: true,
+      actions: [
+        [
+          {
+            key: 'cancel',
+            text: '取消',
+          },
+          {
+            key: 'ok',
+            text: '确认',
+            danger: true,
+          },
+        ],
+      ],
+      onAction: (action) => {
+        if (action.key !== 'ok') return;
+        console.log('actionaa');
+      },
+    });
+  };
+  return (
+    <div className={styles.page}>
+      <div className={styles.bottom}>
+        <div className={styles.btn} onClick={handleSummon}></div>
+        <UserPoints />
+      </div>
+    </div>
+  );
 };
 
 export default Exchange;
