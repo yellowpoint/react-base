599c17199e05e0d09e3ad695c7699244357a1bfa
黄典 <870003719@qq.com>
2024-01-26 15:04:11 +0800

    feat: 使用clip-path实现进度环效果

--- src/pages/My/MyProgress.jsx
+++ src/pages/My/MyProgress.jsx
@@ -1,33 +1,42 @@
-import ProgressCircle from '@/components/ProgressCircle';
+import { useState, useEffect } from 'react';
 
-// import { ProgressCircle } from 'antd-mobile';
+import progressStyles from './progress.module.less';
+
 import styles from './index.module.less';
 
 const MyProgress = ({ limitedNum }) => {
+  const [progressClass, setProgressClass] = useState(0);
+  useEffect(() => {
+    let currentNum = 0;
+    const interval = setInterval(() => {
+      // 检查是否达到限制数
+      if (currentNum < limitedNum) {
+        // 增加进度后缀数字
+        currentNum = currentNum + 1;
+        setProgressClass(currentNum);
+      } else {
+        clearInterval(interval); // 达到限制数，清除计时器
+      }
+    }, 300);
+
+    return () => clearInterval(interval); // 在组件卸载时清除计时器
+  }, [limitedNum]);
   return (
     <div className={styles.norem_progressBar}>
-      <img className={styles.progress0} src="/imgs/my/progress0.png" />
-      <div className={styles.progressCircle}>
-        <ProgressCircle percent={(limitedNum / 12) * 100} />
+      {/* 使用clip-path实现进度环效果 */}
+      <div
+        className={`${styles.progress100Box} ${
+          progressStyles['clip' + progressClass]
+        }`}
+      >
+        <img
+          className={`${styles.progress100}`}
+          src="/imgs/my/progress100.png"
+        />
       </div>
+      <img className={styles.progress0} src="/imgs/my/progress0_2.png" />
       <div className={styles.tips}>{limitedNum}/12</div>
       <img className={styles.icon} src="/imgs/my/progress.png" />
-      {/* <ProgressCircle
-    percent={(limitedNum / 12) * 100}
-    style={{
-      '--size': '80px',
-      '--track-width': '12px',
-      '--track-color': 'transparent',
-      '--fill-color': '#3687d1',
-      // 3687d1
-      // transparent
-    }}
-  >
-  </ProgressCircle> */}
-      {/* <img
-    className={styles.progress100}
-    src="/imgs/my/progress100.png"
-  /> */}
     </div>
   );
 };
